487c7ab049b8257c4aa5043b1916b49cc03d2a16
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-05 11:11:39 +0500

    solved 4

--- find04_max_index.py
+++ find04_max_index.py
@@ -1,8 +1,24 @@
 def find_max_index(data):
+    
     """
     Given the list of numbers, return the index of maximum number in the list
     args:
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    i=0
+    j=0
+    ans = data[0]
+    while i<=len(data)-1:
+        if ans<data[i]:
+            ans = data[i]
+            
+        i+=1
+    g = 0
+    while j<=len(data)-1:
+        if data[j]==ans:
+            g=j
+        j+=1
+    return g
+data = [1,2,3,49,5,8,3]
+print(find_max_index(data))
