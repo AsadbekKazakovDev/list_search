8bf50b9cdaf61b6d3f5eb684eadeb94cefec5306
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-05 11:04:20 +0500

    solved 3

--- find03_max_min_sum.py
+++ find03_max_min_sum.py
@@ -5,4 +5,19 @@ def find_max_min_sum(data):
         data: list of numbers
     returns: sum of the maximum and minimum numbers in the list
     """
-    return 0
+    i=0
+    j=0
+    ans = data[0]
+    while i<=len(data)-1:
+        if ans<data[i]:
+            ans = data[i]
+            
+        i+=1
+    lst=data[0]
+    while j<=len(data)-1:
+        if lst>data[j]:
+            lst=data[j]
+        j+=1
+    return lst+ans
+data = [1,8,3,8,5,8,7,8]
+print(find_max_min_sum(data))
