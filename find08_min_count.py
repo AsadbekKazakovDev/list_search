abada5ebbb7eb092ee0bda85b1af949d6607b35a
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-06 16:38:35 +0500

    solved 8

--- find08_min_count.py
+++ find08_min_count.py
@@ -5,4 +5,20 @@ def find_min_count(data):
         data: list of numbers
     returns: count of minimum numbers in the list
     """
-    return 0
+    i=0
+    ans = data[0]
+    while i<=len(data)-1:
+        
+        if ans>data[i]:
+            ans = data[i]
+        i+=1
+    j = 0
+    k = 0
+    while j<=len(data)-1:
+        if data[j]==ans:
+            k+=1
+        j+=1
+        
+    return k
+data =  [0, -4, 3, 9, -2, -4]
+print(find_min_count(data))
