c50f0cfd156ad162a23b22e8b4e9fd1449bec78c
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-06 16:40:24 +0500

    solved 9

--- find09_min_index.py
+++ find09_min_index.py
@@ -5,5 +5,14 @@ def find_min_index(data):
         data: list of numbers
     returns: index of minimum number in the list
     """
-    return 0
+    i=0
+    lst = []
+    ans = data[0]
+    while i<=len(data)-1:
+        if ans>data[i]:
+            ans = i  
+        i+=1
+    return ans
+data = [2,-9,4,5,6,-97,8,9]
+print(find_min_index(data))
 
